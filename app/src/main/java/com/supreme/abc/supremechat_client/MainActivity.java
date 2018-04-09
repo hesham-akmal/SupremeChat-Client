967333a6193bd1be846c403ad0879bfda2fb0a81
hesham-akmal <hesham.akmal@gmail.com>
2018-04-09 20:28:12 +0200

    .

--- app/src/main/java/com/supreme/abc/supremechat_client/MainActivity.java
+++ app/src/main/java/com/supreme/abc/supremechat_client/MainActivity.java
@@ -15,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
     EditText usernameText, passwordText;
     Button btn;
 
-    public String MainServerIP = "197.52.21.251" ; //HeshamPC
-    //public String MainServerIP = "156.204.165.39" ; //Grey&Ahmad
+    //public String MainServerIP = "197.52.21.251" ; //HeshamPC
+    public String MainServerIP = "156.204.165.39" ; //Grey&Ahmad
     public int MainServerPORT = 3000 ;
 
     //Clicking button
